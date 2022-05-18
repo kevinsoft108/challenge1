fac9b20ddc6b1373862d7fdb39bc938d6f02e2fd
kevinsoft108 <kevinsoft108@gmail.com>
2022-05-18 21:20:40 +0700

    Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function fetchData() {
+  return Math.round(Math.random() * 900);
+}
+
+function getColor(num) {
+  var color;
+  if (1 <= num && num < 300) {
+    color = 'green';
+  } else if (301 <= num && num < 600) {
+    color = 'red';
+  } else {
+    color = 'blue';
+  }
+
+  return color;
+}
 
 function App() {
+  const [color, setColor] = useState(null);
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    var delay = 0;
+    var num;
+    var setButtonColor = function() {
+      if (color) return;
+      num = fetchData();
+      if (num%2 === 1) {
+        delay = delay + 500;
+        setTimeout(setButtonColor, delay);
+      } else {
+        setColor(getColor(num));
+      }
+    }
+
+    setButtonColor();
+  }, [color]);
+
+  function resetColor() {
+    const copy = [...colors];
+    if (color) {
+      copy.unshift(color);
+      setColors(copy);
+    }
+    const newColor = getColor(fetchData());
+    setColor(newColor);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{backgroundColor: color}} onClick={resetColor}>Change Me</button>
+      <ul className="colors">
+        {colors.map((c, i) => <li key={i}>{c}</li>)}
+      </ul>
     </div>
   );
 }
